d314a03d50e534e722af6d06db44d35612af5ec1
drenmorina12 <drenmorina321@gmail.com>
2023-10-02 14:33:51 +0200

    Create player and enemy objects of class Sprite

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const c = canvas.getContext('2d')
 
 const WIDTH = 1024
 const HEIGHT = 576
+const SPRITE_HEIGHT = 150
+const SPRITE_WIDTH = 50
 
 canvas.width = WIDTH
 canvas.height = HEIGHT
@@ -13,4 +15,21 @@ class Sprite {
     constructor(position) {
         this.position = position
     }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, SPRITE_WIDTH, SPRITE_HEIGHT)
+    }
 }
+
+const player = new Sprite({
+    x: 0,
+    y: 0
+})
+
+const enemy = new Sprite({
+    x: 500,
+    y: 200
+})
+
+player.draw()
+enemy.draw()
