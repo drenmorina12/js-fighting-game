ef2d9cb377ed8ac96d08172917dee1d3bb64da50
drenmorina12 <drenmorina321@gmail.com>
2023-10-02 14:21:48 +0200

    Create class Sprite and create constructor

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1024
-canvas.height = 576
+const WIDTH = 1024
+const HEIGHT = 576
+
+canvas.width = WIDTH
+canvas.height = HEIGHT
+
+c.fillRect(0, 0, WIDTH, HEIGHT)
+
+class Sprite {
+    constructor(position) {
+        this.position = position
+    }
+}
