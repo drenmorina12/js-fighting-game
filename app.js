af3bb1986b68cc53b4342ceefada224c328bb69b
drenmorina12 <drenmorina321@gmail.com>
2023-10-02 15:15:39 +0200

    Add update method and call it on animation loop
    use update method to accelerate objects until they hit the ground
    using gravity. Clear rect on everyframe to update it.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const WIDTH = 1024
 const HEIGHT = 576
 const SPRITE_HEIGHT = 150
 const SPRITE_WIDTH = 50
+const GRAVITY = 0.2
 
 canvas.width = WIDTH
 canvas.height = HEIGHT
@@ -12,18 +13,30 @@ canvas.height = HEIGHT
 c.fillRect(0, 0, WIDTH, HEIGHT)
 
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({ position, velocity }) {
         this.position = position
         this.velocity = velocity
+        this.height = SPRITE_HEIGHT
     }
     draw() {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, SPRITE_WIDTH, SPRITE_HEIGHT)
     }
+
+    update() {
+        this.draw()
+
+        this.position.y += this.velocity.y
+        if (this.position.y + this.height + this.velocity.y >= HEIGHT){
+            this.velocity.y = 0
+        } else {
+            this.velocity.y += GRAVITY
+        }
+    }
 }
 
 const player = new Sprite({
-    postion: {  
+    position: {  
         x: 0,
         y: 0
     },
@@ -33,7 +46,6 @@ const player = new Sprite({
     }
 })
 
-player.draw()
 
 const enemy = new Sprite({
     position: {
@@ -46,11 +58,13 @@ const enemy = new Sprite({
     }
 })
 
-enemy.draw()
 
 function animate() {
-
     window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, WIDTH, HEIGHT)
+    player.update()
+    enemy.update()
 }
 
 animate()
